a98adbf06e1cc55c08e755b934ecb0566da63277
Tanu Priya <tanu.priya100701@gmail.com>
2025-02-15 15:35:09 +0530

    first

---
+++ SliceBill/SliceBill/src/Customer.java
@@ -0,0 +1,14 @@
+public class Customer {
+    private String name;
+    private String address;
+
+    // Constructor
+    public Customer(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    // Getters
+    public String getName() { return name; }
+    public String getAddress() { return address; }
+}
